6d30042a3da5cedb83bae8cff0e3588cf99d168c
Alberto Basalo <albertobasalo@hotmail.com>
2015-03-13 14:01:57 +0100

    pi 3 14 15 9 26 53

--- static/app.js
+++ static/app.js
@@ -5,18 +5,15 @@
 		.module('pi-moment', [])
 		.controller("piCtrl", function ($rootScope) {
 			var pi = this;
+			var secsPerDay = 24 * 60 * 60 ;
 			pi.moment = new Date(2015, 2, 14, 9, 26, 53);
 			var socket = io.connect();
 			socket.on("time", function () {
 				$rootScope.$apply(function () {
 					pi.localTime = new Date();
-					if ((pi.moment - pi.localTime) >= 0) {
-						pi.delta = pi.moment.getTime() - pi.localTime.getTime();
-					} else {
-						pi.delta = pi.localTime.getTime() - pi.moment.getTime();
-					}
-					pi.deltaSeconds = ((24 * 60 * 60) -pi.delta) / 1000;
-					pi.progress = pi.deltaSeconds / (24 * 60 * 60);
+					pi.delta = Math.abs(pi.moment.getTime() - pi.localTime.getTime()) ;
+					pi.deltaSeconds = pi.delta /1000 ;
+					pi.progress = 100 * (secsPerDay - pi.deltaSeconds) / secsPerDay ;
 				});
 			});
 		});
